0af580b0362330ddd66b087bdb92eaf266541ea7
valentin Lopez <lopezvalen141@gmail.com>
2023-12-08 11:30:50 -0300

    Entrega Final TP, restan detalles

---
+++ TP1-AW1/JS/productos.js
@@ -0,0 +1,75 @@
+
+const tienda = {
+    articulos: [
+        {
+            nombre: "Remera Air Jordan 1",
+            imagen: "./imagenes/productos/remeras/remera1..webp",
+            precio: 19999,
+        },
+        {
+            nombre: "Remera Nike Unisex",
+            imagen: "./imagenes/productos/remeras/remera2.webp",
+            precio: 20999,
+        },
+        {
+            nombre: "Remera Air Jordan LVL 9",
+            imagen: "./imagenes/productos/remeras/remera3.webp",
+            precio: 25999,
+        },
+        {
+            nombre: "Remera Air Jordan 1",
+            imagen: "./imagenes/productos/remeras/remera4.webp",
+            precio: 27999,
+        },
+        {
+            nombre: "Zapatilla Jordan Mid 1",
+            imagen: "./imagenes/productos/zapatillas/zapatilla1..webp",
+            precio: 119999,
+        },
+
+        {
+            nombre: "Zapatilla Nike Air Force 1 lvl7'",
+            imagen: "./imagenes/productos/zapatillas/zapatilla2..webp",
+            precio: 119999,
+        },
+        {
+            nombre: "Zapatilla Nike Air Force 1 RedWhite",
+            imagen: "./imagenes/productos/zapatillas/zapatilla3..webp",
+            precio: 119999,
+        },
+
+        {
+            nombre: "Zapatilla Nike Air Jordan Retro 4",
+            imagen: "./imagenes/productos/zapatillas/zapatilla7.webp",
+            precio: 199599,
+        },
+
+        {
+            nombre: "Zapatilla Nike Air Jordan Mid Red",
+            imagen: "./imagenes/productos/zapatillas/zapatilla5.webp",
+            precio: 185699,
+        },
+
+        {
+            nombre: "Buzo Canguro Nike Grey",
+            imagen: "./imagenes/productos/abrigos/abrigos1.webp",
+            precio: 185699,
+        },
+
+        {
+            nombre: "Buzo Nike MovingCO Hombre",
+            imagen: "./imagenes/productos/abrigos/abrigos2.webp",
+            precio: 185699,
+        },
+
+        {
+            nombre: "Buzo Canguro Nike Oversized Yellow",
+            imagen: "./imagenes/productos/abrigos/abrigos4.webp",
+            precio: 185699,
+        },
+
+
+    ],
+};
+
+export default tienda
